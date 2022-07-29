6e0a1e380215637778588ea6fa20c91e62cbb8e3
Anuar <anuarnurshinov@gmail.com>
2022-07-29 16:37:31 +0300

    Начальная верстка

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img className='App-logo' src='https://upload.wikimedia.org/wikipedia/commons/a/ab/Android_O_Preview_Logo.png' alt='' />
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div><img src='https://images.wallpaperscraft.ru/image/single/gorodskoj_pejzazh_gorod_vid_sverhu_195543_1280x720.jpg' alt='' /></div>
+            <div> ava + description</div>
+            <div>
+                my posts
+                <div> new post</div>
+                <div>
+                    <div> post 1</div>
+                    <div> post 2</div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Profile;
